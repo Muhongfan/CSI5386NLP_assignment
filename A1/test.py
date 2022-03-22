eefe9729f64a6ed68b55640f97e3260d1060f7e8
MuHongfan <amber1216@live.cn>
2022-03-21 20:55:01 -0400

    updated

--- A1/test.py
+++ A1/test.py
@@ -1,7 +1,8 @@
-import nltk
+# import nltk
+#
+# text = 'That U.S.A POSTER-PRINT COSTS 9% '
+#
+# pattern = r"\w+ |\w+(?:[-']\w+)* "
+# result = nltk.regexp_tokenize(text, pattern= pattern)
+# print(result)
 
-text = 'That U.S.A POSTER-PRINT COSTS 9% '
-
-pattern = r"\w+ |\w+(?:[-']\w+)* "
-result = nltk.regexp_tokenize(text, pattern= pattern)
-print(result)
